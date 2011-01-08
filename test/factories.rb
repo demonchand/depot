ce56438712546b2044faae34eb40a4cae7173e5e
chandru <chandru@xtrafin.net>
2011-01-08 16:41:28 +0530

    insert the factories and write a code in products unit testing

---
+++ test/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :products do |x|
+  x.title "ruby quiz"
+  x.description "bad day bad day bad day bad day bad day"
+  x.image_url "abc@gmail.com"
+  x.price  4
+end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+class ProductTest < Test::Unit::TestCase
+  should_not allow_value("sdfg").for(:image_url)
+  should allow_value(5).for(:price)
+  should allow_value("ruby quiz").for(:title)
 end
+
